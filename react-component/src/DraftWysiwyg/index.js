28dfd21a6c7d9a77b1f6cf0a124d212184d2575e
zhangwei <zhangwei01@unipus.cn>
2020-04-10 16:06:57 +0800

    react-component 完善富文本编辑器react-draftreact-draft-wysiwyg组件图片上传中遇到的问题

--- react-component/src/DraftWysiwyg/index.js
+++ react-component/src/DraftWysiwyg/index.js
@@ -16,6 +16,7 @@ export default () => (
         uploadEnabled: true,
         alignmentEnabled: true,
         previewImage: true,
+        // 当这个返回没有返回promise的时候上传图片会报错，一直显示加载图标 实际开发中这个是用来将图片上传到服务器再用服务器返回的url替换当前编辑器中的图片路径
         uploadCallback: ()=>{},
         inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
         alt: { present: false, mandatory: false },
